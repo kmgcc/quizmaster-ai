fe45865d987ea2a31ba2ff74bf35b8a9035adfb2
kmg <kmgccc@outlook.com>
2026-01-08 20:41:24 +0800

    1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gr83c3klm4g"
+    "revision": "0.ktr8hgt4h78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
